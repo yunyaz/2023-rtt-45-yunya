092d22f01b20fbc4c9290116df1b9ba0245d5995
Yunya Zhang <yunyazhang1986@gmail.com>
2023-02-03 15:34:41 -0500

    first running program

---
+++ src/com/tek/example1/FirstClass.java
@@ -0,0 +1,12 @@
+package com.tek.example1;
+
+public class FirstClass {
+	
+	public static void main(String[] args) {
+		System.out.println("First Java Program.....");
+		
+		int i = 5 + 5;
+		System.out.println("5 + 5 = " + i);
+	}
+
+}
